c727ea3a3af4c1d741968be2a538478c571cd99a
Andrew Hejl <hejlandrew@gmail.com>
2026-01-21 20:41:15 -0700

    Disabled forced user login.

--- src/main/java/com/andrewhejl/plannro/config/Config.java
+++ src/main/java/com/andrewhejl/plannro/config/Config.java
@@ -4,10 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Config {
 
     @Bean
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http
+        .csrf().disable()       //disable csrf
+        .authorizeHttpRequests(auth->auth.anyRequest().permitAll()) //allow any req
+        .httpBasic().disable()  //disable http
+        .formLogin().disable(); //disable form
+        return http.build();
+    }
 }
